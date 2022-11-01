76c2815e748dfb93cbb43f3f95c69ebfd935df94
qalmaqihir <supine.me@gmail.com>
2022-11-01 14:51:04 +0600

    files edited

--- app.py
+++ app.py
@@ -5,43 +5,41 @@ from werkzeug.utils import secure_filename
 from datetime import datetime
 from script import multiprocessing_requests_n_save
 
+
+
+app = Flask(__name__)
+
+
 ALLOWED_EXTENSIONS = set(['csv'])
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def create_app():
-    app = Flask(__name__)
+@app.route("/")
+def index():
+    return redirect(url_for('upload'))
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            # f'output/{datetime.now():%Y-%m-%d_%H-%M-%S}.csv'
+            new_filename = f'{filename.split(".")[0]}_{datetime.now():%Y-%m-%d_%H-%M-%S}.csv'
+            save_location = os.path.join('input', new_filename)
+            file.save(save_location)
 
-    @app.route("/")
-    def index():
-        return redirect(url_for('upload'))
+            output_file = multiprocessing_requests_n_save(save_location)
+            # return send_from_directory('output', output_file)
+            return redirect(url_for('download'))
 
-    @app.route('/upload', methods=['GET', 'POST'])
-    def upload():
-        if request.method == 'POST':
-            file = request.files['file']
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                # f'output/{datetime.now():%Y-%m-%d_%H-%M-%S}.csv'
-                new_filename = f'{filename.split(".")[0]}_{datetime.now():%Y-%m-%d_%H-%M-%S}.csv'
-                save_location = os.path.join('input', new_filename)
-                file.save(save_location)
+    return render_template('upload.html')
 
-                output_file = multiprocessing_requests_n_save(save_location)
-                # return send_from_directory('output', output_file)
-                return redirect(url_for('download'))
+@app.route('/download')
+def download():
+    return render_template('download.html', files=os.listdir('output'))
 
-        return render_template('upload.html')
-
-    @app.route('/download')
-    def download():
-        return render_template('download.html', files=os.listdir('output'))
-
-    @app.route('/download/<filename>')
-    def download_file(filename):
-        return send_from_directory('output', filename)
-
-    return app
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory('output', filename)
